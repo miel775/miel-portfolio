14606bb00a924dd0de7941de14dfc613b7f8089d
Miel <miel.van.sleeuwen.van.sleeuwen@hva.nl>
2026-01-16 11:52:43 +0100

    build: code
    All the code is collected for the background color changing
    
    #13

--- my-react-app/src/components/Banner.jsx
+++ my-react-app/src/components/Banner.jsx
@@ -13,6 +13,28 @@ const time = hours + minutes / 60;
 let hue = 210;
 let lightness = 78;
 
+if (time >= 8 && time < 8.5) {
+    const progress = (time - 8) / 0.5;
+    hue = 236 + (210 - 236) * progress;
+    lightness = 18 + (78 - 18) * progress;
+}
+
+  else if (time >= 19 && time < 19.5) {
+    const progress = (time - 19) / 0.5;
+    hue = 210 + (236 - 210) * progress;
+    lightness = 78 + (18 - 78) * progress;
+  }
+
+  else if (time >= 19.5 || time < 8) {
+    hue = 236;
+    lightness = 18;
+  }
+
+  else {
+    hue = 210;
+    lightness = 78;
+  }
+
 // hsl should be 233 from 19:30 untill 8:30
 
 // hsl color is 236/210, 78%, 78%/18%
