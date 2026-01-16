f3ef210ca2aee2a5f74ab0320f8a78b6f184d7f0
Miel <miel.van.sleeuwen.van.sleeuwen@hva.nl>
2026-01-16 11:45:51 +0100

    feat: userstory
    user story described
    
    #13

--- my-react-app/src/components/Banner.jsx
+++ my-react-app/src/components/Banner.jsx
@@ -9,7 +9,12 @@ console.log(currentDate.getMinutes())
 let minutesNow = currentDate.getMinutes()
 let hoursNow = currentDate.getHours()
 
-// when the clock hits 8:00 hsl should be 
+// hsl should be 233 from 19:30 untill 8:30
+
+// hsl color is 236/210, 78%, 78%/18%
+
+// between 8:00 and 8:30 hue should change from 236 to 210 and lightness from 18% to 78% precent
+// between 19:00 and 19:30 the hue should change from 210 to 236 and lightness from 78% to 18% precent
 
 
 
@@ -17,7 +22,7 @@ let hoursNow = currentDate.getHours()
 
 function Banner() {
   return (
-    <div id="banner-portfolio" style="color" >
+    <div id="banner-portfolio" style="background-color=hsl{variableHue},78%,{variableLightness}" >
 
         
     </div>
