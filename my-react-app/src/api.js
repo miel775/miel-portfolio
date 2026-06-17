2166ff9d2cb5cc4745e065c4acd75f7a55f5476f
Miel <miel.van.sleeuwen.van.sleeuwen@hva.nl>
2026-06-17 14:10:54 +0200

    build: env varibales
    env for the databse

--- my-react-app/src/api.js
+++ my-react-app/src/api.js
@@ -1,2 +1,1 @@
-// src/api.js
-export const STRAPI_URL = (typeof globalThis !== 'undefined' && globalThis.process && globalThis.process.env && globalThis.process.env.REACT_APP_STRAPI_URL) || 'http://localhost:1337';
+export const STRAPI_URL = process.env.REACT_APP_STRAPI_URL || 'http://localhost:1337';
